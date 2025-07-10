065522294b01b6663f458ecd485621bf3d8f5bf0
FanyaOff <73064979+FanyaOff@users.noreply.github.com>
2025-07-10 17:33:10 +0300

    Initial commit

---
+++ src/main/java/com/fanya/enchantmentcalculator/EnchantmentCalculatorMod.java
@@ -0,0 +1,10 @@
+package com.fanya.enchantmentcalculator;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EnchantmentCalculatorMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/fanya/enchantmentcalculator/client/EnchantmentCalculatorClient.java
@@ -0,0 +1,10 @@
+package com.fanya.enchantmentcalculator.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnchantmentCalculatorClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
